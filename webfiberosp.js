6714eb47ecc295d1531b717003eb20177772ef91
dave logan <davel@kci.net>
2023-07-09 04:46:23 -0600

    layout/size changing, started grabbing events

--- webfiberosp.js
+++ webfiberosp.js
@@ -16,10 +16,12 @@ function PutMapOnPage() {
 	ResizeWindow();
 	map = L.map('mapdiv').setView([33.6049777,-112.4547048], 8);
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    	maxZoom: 18,
+    	maxZoom: 19,
     	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
 	map.on('click', function(e) { MapClick(e); });
+	map.on('doubleclick', function(e) { MapDoubleClick(e); });
+	map.on('mousemove', function(e) { PointerMove(e); });
 }
 
 // don't resize immediately, in case we're mid-resize
@@ -45,5 +47,24 @@ function ResizeWindow() {
 
 function MapClick(e) {
 	console.log("map click on lat ["+e.latlng.lat+"] lon ["+e.latlng.lng+"]");
+}
+
+function MapDoubleClick(e) {
+	console.log("map doubleclick on lat ["+e.latlng.lat+"] lon ["+e.latlng.lng+"]");
+}
+
+function PointerMove(e) {
+	// seems like a lot of work, maybe set a flag
+	// console.log("pointer at lat ["+e.latlng.lat+"] lon ["+e.latlng.lng+"]");
+	tmplat = e.latlng.lat.toString().split('.');
+	lat = tmplat[0]+'.'+tmplat[1].substring(0,6);
+	tmplon = e.latlng.lng.toString().split('.');
+	lon = tmplon[0]+'.'+tmplon[1].substring(0,6);
+	$("#displat").html(lat);
+	$("#displon").html(lon);
+}
+
+function MoveMap() {
+	// use getCenter() and getZoom() to populate the info fields
 }
 
